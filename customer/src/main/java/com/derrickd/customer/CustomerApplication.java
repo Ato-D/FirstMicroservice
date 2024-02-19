bd8017de9645a7d332b1725c4840c002ecfa9143
Ato-D <donkorderrick31@gmail.com>
2024-02-18 22:21:08 -0800

    Registered the customer and the fraud services on the Eureka Server

--- customer/src/main/java/com/derrickd/customer/CustomerApplication.java
+++ customer/src/main/java/com/derrickd/customer/CustomerApplication.java
@@ -2,6 +2,9 @@ package com.derrickd.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
 
 @SpringBootApplication
 public class CustomerApplication {
